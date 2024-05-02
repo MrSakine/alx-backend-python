4ab3ff953e17b167cf2afab2294534369b687ceb
MrSakine <msakine20@gmail.com>
2024-05-02 15:10:43 +0000

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Define variables"""
+
+
+if __name__ == "__main__":
+    a: int = 1
+    pi: float = 3.14
+    i_understand_annotations: bool = True
+    school: str = "Holberton"
