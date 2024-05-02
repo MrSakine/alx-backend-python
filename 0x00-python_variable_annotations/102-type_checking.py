1962309f229a436236cc1b0b9426e0a3882a6267
MrSakine <msakine20@gmail.com>
2024-05-02 17:09:42 +0000

    code update

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,7 +4,7 @@ import typing
 
 
 def zoom_array(
-        lst: typing.Sequence[typing.Any],
+        lst: typing.Tuple,
         factor: int = 2
         ) -> typing.List:
     """Type Checking"""
@@ -12,6 +12,6 @@ def zoom_array(
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 zoom_2x = zoom_array(array)
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
