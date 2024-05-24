816ec614fca3f684a9f968e8b293066e477055b0
MrSakine <msakine20@gmail.com>
2024-05-24 16:26:07 +0000

    typing update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from .utils import access_nested_map
-from typing import Sequence
+from typing import Dict, Tuple, Union
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,7 +13,12 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, map: Sequence, path: Sequence, result: int):
+    def test_access_nested_map(
+        self,
+        map: Dict,
+        path: Tuple[str],
+        result: Union[Dict, int]
+    ):
         """ Test access nested map method"""
         self.assertEqual(access_nested_map(map, path), result)
 
