60f52902ab5a91fdc6ed9adc7827300c035f9a13
MrSakine <msakine20@gmail.com>
2024-05-24 16:45:16 +0000

    Test `access_nested_map_exception` method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,13 +11,26 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(
         self,
         map: Dict,
-        nested_map: Tuple[str],
+        path: Tuple[str],
         expected: Union[Dict, int]
     ) -> None:
-        """ Test access nested map method"""
-        self.assertEqual(access_nested_map(map, nested_map), expected)
+        """ Test `access_nested_map` method """
+        self.assertEqual(access_nested_map(map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(
+        self,
+        map: Dict,
+        path: Tuple[str]
+    ) -> None:
+        """ Test `access_nested_map_exception` method """
+        with self.assertRaises(KeyError):
+            access_nested_map(map, path)
